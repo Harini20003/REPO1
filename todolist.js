0d96b6bf0eea602a0dddad9967ebf1b3e49d981b
Harini20003 <143331221+Harini20003@users.noreply.github.com>
2023-08-27 19:11:02 +0530

    Add files via upload

---
+++ todolist.js
@@ -0,0 +1,23 @@
+const taskInput = document.getElementById('taskInput');
+const addButton = document.getElementById('addButton');
+const taskList = document.getElementById('taskList');
+
+addButton.addEventListener('click', addTask);
+
+function addTask() {
+    const taskText = taskInput.value.trim();
+    if (taskText !== '') {
+        const li = document.createElement('li');
+        li.innerHTML = `
+            ${taskText}
+            <button class="deleteButton">Delete</button>
+        `;
+        taskList.appendChild(li);
+        taskInput.value = '';
+
+        const deleteButton = li.querySelector('.deleteButton');
+        deleteButton.addEventListener('click', () => {
+            taskList.removeChild(li);
+        });
+    }
+}
